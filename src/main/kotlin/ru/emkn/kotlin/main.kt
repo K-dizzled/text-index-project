8bff7bc26e442bafcee62e88bac4c61ece2b232d
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-10-11 05:22:06 +0300

    Do Task1

--- src/main/kotlin/ru/emkn/kotlin/main.kt
+++ src/main/kotlin/ru/emkn/kotlin/main.kt
@@ -1,6 +1,10 @@
 package ru.emkn.kotlin
 
+import java.io.File
+import kotlin.collections.HashSet
+
 fun main(args: Array<String>) {
+    val wordToBeFound = "наташин"
     fun Trie<Char>.findIndex(string: String): Long {
         return findIndex(string.toList())
     }
@@ -11,7 +15,32 @@ fun main(args: Array<String>) {
 
     val trie = parseCSV()
 
-    println("\nСловоформы слова \"коряга\"")
-    println(trie.listForms("коряга", trie.findIndex("коряга")))
+    println(trie.listForms(wordToBeFound, trie.findIndex(wordToBeFound)))
+    val answer1= findWordInText(trie.listForms(wordToBeFound, trie.findIndex(wordToBeFound)).toList())
+    println("Страницы, на которых найдены формы слова \"$wordToBeFound\": ${answer1.joinToString()}")
 }
 
+fun findWordInText(wordForms: List<String>, fileName: String = "./data/Childhood.txt"): HashSet<Int> {
+    var pageIndex = 1
+    var lineIndex = 1
+    var formsCounter : Long = 0
+    val pagesWhereWordFormAppears = hashSetOf<Int>()
+    val regx = Regex("[^А-Яа-яA-Za-z0-9]")
+    File(fileName).forEachLine { line ->
+        if (line.trim() != "") {
+            val wordList: List<String> = line.trim().split(" ")
+            wordList.forEach {
+                if (wordForms.contains(regx.replace(it, "").toLowerCase())) {
+                    pagesWhereWordFormAppears.add(pageIndex)
+                    formsCounter++
+                }
+            }
+            lineIndex++
+            if(lineIndex == 45) {
+                lineIndex = 1
+                pageIndex++
+            }
+        }
+    }
+    return pagesWhereWordFormAppears
+}
