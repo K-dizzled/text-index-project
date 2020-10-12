7b11528904ad8928b8b63e6671fd11a9e3203d55
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-10-13 00:32:21 +0300

    Add used words divided into categories

--- src/main/kotlin/ru/emkn/kotlin/main.kt
+++ src/main/kotlin/ru/emkn/kotlin/main.kt
@@ -31,6 +31,11 @@ fun main(args: Array<String>) {
     findWordInText(dict.listForms(wordToBeFound, dict.findIndex(wordToBeFound)), task = 2)
     //Print 5 most used words from the text
     printMostUsedWords(dict)
+    //Print all words and it's forms contained in some category
+    val category = "прилагательные"
+    println("Формы, в которых встречаются слова из категории \"$category\": " +
+            findWordsFromCategory(category, dict).joinToString()
+    )
 }
 
 fun findWordInText(wordForms: List<String>, fileName: String = "./data/Childhood.txt", task: Int = 1)
@@ -107,4 +112,38 @@ fun printMostUsedWords(dict: Trie<Char>) {
         word++
     }
     println("Топ 5 самых часто встречающихся в тексте слов: ${topOfWords.joinToString()}")
+}
+
+fun findWordsFromCategory(category: String, dict: Trie<Char>, fileName: String = "./data/Childhood.txt")
+        : HashSet<String> {
+
+    fun Trie<Char>.findIndex(string: String): Long {
+        return findIndex(string.toList())
+    }
+
+    fun Trie<Char>.listForms(prefix: String, wordIndex: Long): List<String> {
+        return listForms(prefix.substring(0,(prefix.length / 2)).toList(), wordIndex)
+            .map { it.joinToString(separator = "") }
+    }
+    val wordsFromCategory = hashSetOf<String>()
+    var neededList = listOf<String>()
+    File("./data/categories.txt").forEachLine { Category ->
+           if(Category.startsWith(category)) {
+               neededList = Category.substringAfter(": ").trim().split(" ")
+        }
+    }
+    val regx = Regex("[^А-Яа-яA-Za-z0-9]")
+    val wordForms = mutableListOf<String>()
+    for(word in neededList) {
+        wordForms.addAll(dict.listForms(word, dict.findIndex(word)))
+    }
+    File(fileName).forEachLine { line ->
+        val wordList: List<String> = line.trim().split(" ")
+        wordList.forEach {
+            if (wordForms.contains(regx.replace(it, "").toLowerCase())) {
+                wordsFromCategory.add(regx.replace(it, "").toLowerCase())
+            }
+        }
+    }
+    return wordsFromCategory
 }
